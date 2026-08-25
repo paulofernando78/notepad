17487cf3e3b05fff65ceb6a5c40f0b5c28fc2431
Paulo Silva <paulofernando78@gmail.com>
2026-08-25 16:35:26 -0300

    wip

--- src/components/ui/TimerControls/TimerControls.jsx
+++ src/components/ui/TimerControls/TimerControls.jsx
@@ -12,32 +12,32 @@ export const TimerControls = ({
   className,
 }) => {
   return (
-    <div className={`flex gap-2 mt-2 mb-1 ${className}`}>
+    <div className={`flex gap-4 mt-2 mb-1 ${className}`}>
       <button
         onClick={onToggle}
         disabled={toggleDisabled}
         className="disabled:cursor-not-allowed disabled:opacity-40"
       >
         {isRunning ? (
-          <Icon name="circlePause" size="25" />
+          <Icon name="circlePause" size="27" />
         ) : (
-          <Icon name="circlePlay" size="25" />
+          <Icon name="circlePlay" size="27" />
         )}
       </button>
 
       <button onClick={onReset}>
-        <Icon name="rotateCcw" size="25" />
+        <Icon name="rotateCcw" size="27" />
       </button>
 
       {showEdit && (
         <>
           {isEditing ? (
             <button onClick={onConfirmEdit}>
-              <Icon name="check" size="23" className="translate-y-[0.04rem]" />
+              <Icon name="check" size="25" className="translate-y-[0.04rem]" />
             </button>
           ) : (
             <button onClick={onEdit}>
-              <Icon name="pencil" size="23" />
+              <Icon name="pencil" size="25" />
             </button>
           )}
         </>
