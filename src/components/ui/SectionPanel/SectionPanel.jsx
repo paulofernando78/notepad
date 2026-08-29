e2192c839987144d210f5ba8f370630da6caae4b
Paulo Silva <paulofernando78@gmail.com>
2026-08-29 15:57:42 -0300

    wip

--- src/components/ui/SectionPanel/SectionPanel.jsx
+++ src/components/ui/SectionPanel/SectionPanel.jsx
@@ -1,9 +1,6 @@
-import { useId } from "react"
+import { useId } from "react";
 
-export const SectionPanel = ({
-  title,
-  children,
-}) => {
+export const SectionPanel = ({ title, children }) => {
   const headingId = useId();
 
   return (
@@ -19,18 +16,23 @@ export const SectionPanel = ({
     >
       <header
         className="
-        block
-        pb-2
-        text-lg
-        font-bold
-        uppercase
-      "
+          block
+          pb-2
+          text-lg
+          font-bold
+          uppercase
+        "
       >
-        <h2>
-          {title}
-        </h2>
+        <h2>{title}</h2>
       </header>
-      <div className="flex gap-2 bg-gray-100/10 p-2 rounded-lg">
+      <div
+        className="
+          flex gap-2
+          bg-gray-100/10
+          p-2
+          rounded-lg
+        "
+      >
         {children}
       </div>
     </section>
