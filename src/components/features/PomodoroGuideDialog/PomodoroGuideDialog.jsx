9da875e5d3a4f3820571cbbde506338fe0182604
Paulo Silva <paulofernando78@gmail.com>
2026-08-30 23:32:05 -0300

    wip

--- src/components/features/PomodoroGuideDialog/PomodoroGuideDialog.jsx
+++ src/components/features/PomodoroGuideDialog/PomodoroGuideDialog.jsx
@@ -4,18 +4,16 @@ import { Icon } from "@/components/ui/Icon";
 import { Dialog } from "../../ui/Dialog/Dialog";
 
 const phaseColors = {
-  focus: "text-green-500",
-  break: "text-yellow-500",
-  long: "text-blue-500",
-  done: "text-red-500",
+  focus:
+    "text-green-500 font-['Oswald_Variable'] self-center justify-self-center",
+  break:
+    "text-yellow-500 font-['Oswald_Variable'] self-center justify-self-center",
+  long: "text-blue-500 font-['Oswald_Variable'] self-center justify-self-center",
+  done: "text-red-500 font-['Oswald_Variable'] self-center justify-self-center",
 };
 
 const PhaseLabel = ({ phase }) => {
-  return (
-    <span className={`${phaseColors[phase]} uppercase`}>
-      {phase}
-    </span>
-  );
+  return <span className={`${phaseColors[phase]} uppercase`}>{phase}</span>;
 };
 
 const Tomate = ({ className }) => {
@@ -25,7 +23,7 @@ const Tomate = ({ className }) => {
       height="28"
       src="https://img.icons8.com/emoji/96/tomato-emoji.png"
       alt="tomato-emoji"
-      className={`translate-y-[-0.2rem] ${className}`}
+      className={className}
     />
   );
 };
@@ -43,7 +41,7 @@ export const PomodoroGuideDialog = () => {
         <Icon name="info" />
       </button>
       <Dialog dialogRef={dialogRef} className="space-y-2">
-        <h3 className="font-bold">What is it?</h3>
+        <h3 className="font-bold uppercase">What is it?</h3>
         <p>A simple method to balance focus with deliberate breaks.</p>
         {/* 1 Focus */}
         <p>
@@ -51,36 +49,32 @@ export const PomodoroGuideDialog = () => {
           1980s. He created the method as a university student because he
           struggled to focus on his studies and avoid distractions.
         </p>
-        <h3 className="font-bold">How does it works?</h3>
+        <h3 className="font-bold uppercase">How does it works?</h3>
         <ol className="space-y-3">
           <li>
-            1. Plan your tasks.
+            <span className="font-bold">1. Plan your tasks.</span>
             <p>How many pomodoros might you need?</p>
           </li>
           <li>
-            2. Do 1 FOCUS (Pomodoro)
+            <span className="font-bold">2. Do 1 FOCUS (Pomodoro)</span>
             <p>Time for 25 minutes then take a 5 minute break.</p>
           </li>
           <div
             className="
               grid grid-cols-[1fr_auto_1fr_auto_auto]
               grid-rows-[auto_auto]
-              gap-x-2
-              pl-2
-              pr-1
-              pt-2
-              pb-[0.4rem]
+              gap-x-1
+              px-2
               w-max
-              border
-              border-red-100
+              
               rounded-lg
             "
           >
             <PhaseLabel phase="focus" />
-            <Icon name="arrowRight" aria-hidden="true" />
+            <Icon name="arrowRight" aria-hidden="true" className="self-center"/>
             <PhaseLabel phase="break" />
             <span className="self-center">=</span>
-            <Tomate />
+            <Tomate className="relative top-[-0.01rem] left-1"/>
             <div className="self-center justify-self-center text-sm">
               25mins
             </div>
@@ -93,7 +87,7 @@ export const PomodoroGuideDialog = () => {
             3. Repeat{" "}
             <span className="flex">
               4x
-              <Tomate className="ml-1" />,
+              <Tomate className="ml-1 relative top-[-0.2rem]" />,
             </span>
             then take a long break.
           </li>
