9efdc43fbb01d2816388c09588cdefe30a9da523
Paulo Silva <paulofernando78@gmail.com>
2026-09-03 16:33:25 -0300

    wip

--- src/components/features/QuickNotes/QuickNotes.jsx
+++ src/components/features/QuickNotes/QuickNotes.jsx
@@ -31,6 +31,7 @@ export const QuickNotes = ({ note = "", onConfigChange, onRemove }) => {
               p-3
               text-gray-700
               font-['Indie_Flower',cursive]
+              leading-5
               bg-gradient-to-tl from-yellow-100 via-yellow-200 to-yellow-300
               border-0
               resize-none
